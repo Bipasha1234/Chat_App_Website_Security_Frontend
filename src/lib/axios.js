6b4b80101dfeead3ce62d858f9fc6715c3d04c45
bipasha <bipashalamsal@gmail.com>
2025-07-25 22:41:34 +0545

    port of backend added in frontend for connection with each other using axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api"
+    ? "http://localhost:4000/api"
     : "/api";
 
 export const axiosInstance = axios.create({
